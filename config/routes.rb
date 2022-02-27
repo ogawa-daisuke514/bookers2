8a0c9a42bcd19fb1fb1265bc31192d7a90c25fee
Ogawa Daisuke <99077817+ogawa-daisuke514@users.noreply.github.com>
2022-02-27 06:48:54 +0000

    [Finish] ファイルの準備と設定まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'homes/about'
+  devise_for :users
+  resources :users, only: ["index", "show", "edit", "update"]
+  resources :books, only: ["index", "create", "show", "edit", "update", "destroy"]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
